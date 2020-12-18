a0f645aec12e97e3dad9092131e7f6a2d3926ce7
Nat <davidson@cloudflare.com>
2020-12-18 16:27:39 -0500

    add source

---
+++ gatsby-config.js
@@ -0,0 +1,85 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Nat Davidson`,
+    siteTitleAlt: `Nat Davidson`,
+    siteDescription: `Nat's landing page`,
+        siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+        // Will be used to generate absolute URLs for og:image etc.
+        siteUrl: `https://minimal-blog.lekoarts.de`,
+        // Used for SEO
+        siteDescription: `Nat's landing page.`,
+        // Will be set on the <html /> tag
+        siteLanguage: `en`,
+        // Used for og:image and must be placed inside the `static` folder
+        siteImage: `/banner.jpg`,
+        // Twitter Handle
+        author: `@lekoarts_de`,
+        // Links displayed in the header on the right side
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+
+        navigation: [
+
+        ],
+        externalLinks: [
+          {
+            name: `Github`,
+            url: `https://github.com/nataliescottdavidson`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  
+  ].filter(Boolean),
+}
